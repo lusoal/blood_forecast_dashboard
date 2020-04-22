ae8ad69a768b4221ed896d6bc5cb30f175feef1b
lusoal <lucas.asduarte@gmail.com>
2020-04-22 12:43:05 -0300

    feat(initial) initial commit

--- app.py
+++ app.py
@@ -1,99 +1,163 @@
-from flask import Flask, render_template, jsonify, \
-    request, Response, redirect, url_for, session
+#!/usr/bin/python
+
+from flask import (
+    Flask,
+    render_template,
+    # jsonify,
+    request,
+    # Response,
+    redirect,
+    url_for,
+    session,
+)
+import logging
 import os
 import requests
 import datetime
 import json
+import sys
 
 from congito import cognito_auth
 
 app = Flask(__name__)
-app.secret_key = 'eipohgoo4rai0uf5ie1oshahmaeF'
+app.secret_key = "eipohgoo4rai0uf5ie1oshahmaeF"
 
 # Constants
-USER_POOL_ID = "us-east-1_QStEStwkt"
-APP_CLIENT_ID = "4mo24hht37mvcfo0go8t3bn1qa"
-API_GATEWAY_URL_ENDPOINT = "https://1t27fdgq0j.execute-api.us-east-1.amazonaws.com/prd"
+USER_POOL_ID = os.getenv("USER_POOL_ID", "")
+
+APP_CLIENT_ID = os.getenv("APP_CLIENT_ID", "")
+APP_CLIENT_SECRET = os.getenv("APP_CLIENT_SECRET", "")
+
+API_GATEWAY_URL_ENDPOINT = os.getenv("API_GATEWAY_URL_ENDPOINT", "")
+COGNITO_URL = os.getenv("COGNITO_URL", "")
+
 
 # Routes of my application
-@app.route('/')
+@app.route("/")
 def index():
-    # session['user_id'] = usuario.id Set Session attribute
-    if not session.get('app_client_id'):
-        return render_template("login.html")
+    if not session.get("app_client_id"):
+        return redirect(COGNITO_URL, code=302)
     return redirect(url_for("menu"))
 
 
-@app.route('/login', methods=["POST"])
+@app.route("/login_callback", methods=["GET"])
+def login_callback():
+    code = request.args.get("code")
+    endpoint = (
+        "https://blood-analysis-test.auth.us-east-1.amazoncognito.com/oauth2/token"
+    )
+
+    headers = {}
+    headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+    data = {}
+    data["grant_type"] = "authorization_code"
+    data["client_id"] = APP_CLIENT_ID
+    data["code"] = code
+    data["redirect_uri"] = "https://dashboard.lucasduarte.club/login_callback"
+
+    response = requests.post(
+        endpoint, headers=headers, data=data, auth=(APP_CLIENT_ID, APP_CLIENT_SECRET)
+    )
+
+    # Todo tratar os erros
+    response_dict = json.loads(response.text)
+
+    logging.warning(f"Autenticacao retorno {response_dict}")
+
+    session["app_client_id"] = response_dict.get("id_token")
+    session["access_token"] = response_dict.get("access_token")
+    session[
+        "user_id"
+    ] = (
+        1
+    )  # TODO: pegar user_id do cognito, criar atributo obrigatorio chamado device_id
+
+    return redirect(url_for("menu"))
+
+
+@app.route("/login", methods=["POST"])
 def login():
     # session['user_id'] = usuario.id Set Session attribute
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form["username"]
+    password = request.form["password"]
     try:
-        app_client_id = cognito_auth.authenticate_and_get_token(username, password, USER_POOL_ID, APP_CLIENT_ID)
+        app_client_id = cognito_auth.authenticate_and_get_token(
+            username, password, USER_POOL_ID, APP_CLIENT_ID
+        )
 
-        session['app_client_id'] = app_client_id['IdToken']
-        session['user_id'] = 1
+        session["app_client_id"] = app_client_id["IdToken"]
+        session["user_id"] = 1
 
         return redirect(url_for("menu"))
     except Exception as e:
+        print(f"[ERROR] {e}", file=sys.stderr)
         return redirect(url_for("index"))
 
 
-@app.route('/logout', methods=["POST"])
+@app.route("/logout", methods=["POST"])
 def logout():
     session.clear()
     return redirect(url_for("index"))
 
 
-@app.route('/menu', methods=["GET"])
+@app.route("/menu", methods=["GET"])
 def menu():
     return render_template("menu.html")
 
 
-@app.route('/realizar_forecast', methods=["GET"])
+@app.route("/realizar_forecast", methods=["GET"])
 def realizar_forecast():
     # TODO: validate response if contains error message
     try:
-        headers = {'X-COG-ID': session['app_client_id']}
-        payload = {'user_id': session['user_id']}
-        response = requests.get(f"{API_GATEWAY_URL_ENDPOINT}/get_blood_forecast", params=payload, headers=headers)
+        headers = {"X-COG-ID": session["app_client_id"]}
+        payload = {"user_id": session["user_id"]}
+        response = requests.get(
+            f"{API_GATEWAY_URL_ENDPOINT}/get_blood_forecast",
+            params=payload,
+            headers=headers,
+        )
 
         forecast = response.text
+        logging.warning(f"Forecast Retorno {forecast}")
         return redirect(url_for("exibir_grafico_forecast", forecast=forecast))
     except Exception as e:
+        print(f"[ERROR] {e}", file=sys.stderr)
         return redirect(url_for("menu"))
 
 
-@app.route('/exibir_grafico_forecast', methods=["GET", "POST"])
+@app.route("/exibir_grafico_forecast", methods=["GET", "POST"])
 def exibir_grafico_forecast():
-    forecast = json.loads(request.args['forecast'])
+    forecast = json.loads(request.args["forecast"])
     list_glicose = []
-    day_of_analysis = None
 
-    for time, glicose in zip(forecast['time'], forecast['glicose']):
+    time = forecast["time"][1].replace("T", " ")
+    time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+    day_of_analysis = f"{time.day}/{time.month}/{time.year}"
+
+    for time, glicose in zip(forecast["time"], forecast["glicose"]):
         new_dict = {}
 
         time = time.replace("T", " ")
-        time = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+        time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
         timestamp = int(time.timestamp() * 1000)
-        new_dict['x'] = timestamp
-        new_dict['y'] = glicose
+        new_dict["x"] = timestamp
+        new_dict["y"] = glicose
         list_glicose.append(new_dict)
-        day_of_analysis = f"{time.day}/{time.month}/{time.year}"
-    
-    glic = list(item['y'] for item in list_glicose)
+
+    glic = list(item["y"] for item in list_glicose)
 
     glic_min = min(glic)
     glic_max = max(glic)
 
-    return render_template("grafico_forecast.html", list_glicose=list_glicose, 
-        day_of_analysis=day_of_analysis, glic_min=glic_min, glic_max=glic_max)
+    return render_template(
+        "grafico_forecast.html",
+        list_glicose=list_glicose,
+        day_of_analysis=day_of_analysis,
+        glic_min=glic_min,
+        glic_max=glic_max,
+    )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
-
-
-# TODO: Create user authentication
-# cognito_auth.create_user("lucasduarte", "V@itomanocu1", USER_POOL_ID, APP_CLIENT_ID, "lucasdu@amazon.com")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- congito/cognito_auth.py
+++ congito/cognito_auth.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
 import boto3
 
+
 def create_user(username, password, user_pool_id, app_client_id, email):
-    client = boto3.client('cognito-idp')
+    client = boto3.client("cognito-idp")
 
     try:
         # initial sign up
@@ -9,38 +11,27 @@ def create_user(username, password, user_pool_id, app_client_id, email):
             ClientId=app_client_id,
             Username=username,
             Password=password,
-            UserAttributes=[
-                {
-                    'Name': 'email',
-                    'Value': email
-                },
-            ]
+            UserAttributes=[{"Name": "email", "Value": email}],
         )
         # then confirm signup
-        resp = client.admin_confirm_sign_up(
-            UserPoolId=user_pool_id,
-            Username=username
-        )
+        resp = client.admin_confirm_sign_up(UserPoolId=user_pool_id, Username=username)
+        print(f"[INFO] {resp}")
         return True
     except Exception as e:
         raise e
 
-    
 
 def authenticate_and_get_token(username, password, user_pool_id, app_client_id):
-    client = boto3.client('cognito-idp')
+    client = boto3.client("cognito-idp")
     try:
         resp = client.admin_initiate_auth(
             UserPoolId=user_pool_id,
             ClientId=app_client_id,
-            AuthFlow='ADMIN_NO_SRP_AUTH',
-            AuthParameters={
-                "USERNAME": username,
-                "PASSWORD": password
-            }
+            AuthFlow="ADMIN_NO_SRP_AUTH",
+            AuthParameters={"USERNAME": username, "PASSWORD": password},
         )
         print(f"INFO: {resp['AuthenticationResult']}")
 
-        return resp['AuthenticationResult']
+        return resp["AuthenticationResult"]
     except Exception as e:
         raise e
